3e5b25fb08d4270d6ffb3463cb2f844fc00bb779
Navid Sedehi <navid.sedehi@gmail.com>
2022-09-25 18:29:25 +0330

    wip

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,33 +1,41 @@
 @extends('tabler::layout')
-
 @section('content')
-	<form action="" method="post" class="card">
-		<div class="card-header">
-			<h4 class="card-title">Form elements</h4>
-		</div>
-		<div class="card-body">
-			<div class="row">
-				<x:form::input placeholder="salam" class="date" name="navid"/>
-				<x:form::input placeholder="salam" Disabled name="navid"/>
-				<x:form::input type="password" placeholder="salam" name="navid"/>
-				<x:form::checkbox name="newsletter_subscription" :checked="true"/>
-				@php($options = [1 => 'Laravel', 2 => 'Bootstrap', 3 => 'Tailwind', 4 => 'Livewire'])
-				<x:form::select name="hobby" placeholder="What is your hobby prefered hobby?" :options="$options" selected="1"/>
-				<x:form::select name="hobbies" :options="$options" :selected="[2, 3]" allowPlaceholderToBeSelected multiple/> {{-- You'll be able to selected the placeholder and the name will be converted to hobbies[] --}}
-				<x:form::checkbox name="newsletter_subscription" :checked="true"/>
-				<x:form::toggle-switch name="active" :checked="false" inline/>
-
-				<x:form::textarea id="custom-id" name="first_name"/>
-				<x:form::input name="name" caption="Please fill this input with your full name." />
+	<div class="page-header">
+		<div class="row align-items-center">
+			<div class="col">
+				<div class="page-pretitle">
+					Overview
+				</div>
+				<h2 class="page-title">
+					Dashboard
+				</h2>
 			</div>
 		</div>
-		<div class="card-footer text-end">
-			<div class="d-flex">
-				<button type="submit" class="btn btn-primary ms-auto">Send data</button>
-				<a href="#" class="btn btn-link">Cancel</a>
+	</div>
+	<div class="col-12">
+		<form action="" method="post" class="card">
+			<div class="card-body">
+				<div class="row">
+					<x:form::input placeholder="salam" class="date" name="navid"/>
+					<x:form::input placeholder="salam" Disabled name="navid"/>
+					<x:form::input type="password" placeholder="salam" name="navid"/>
+					<x:form::checkbox name="newsletter_subscription" :checked="true"/>
+					@php($options = [1 => 'Laravel', 2 => 'Bootstrap', 3 => 'Tailwind', 4 => 'Livewire'])
+					<x:form::select name="hobby" placeholder="What is your hobby prefered hobby?" :options="$options" selected="1"/>
+					<x:form::select name="hobbies" :options="$options" :selected="[2, 3]" allowPlaceholderToBeSelected multiple/> {{-- You'll be able to selected the placeholder and the name will be converted to hobbies[] --}}
+					<x:form::checkbox name="newsletter_subscription" :checked="true"/>
+					<x:form::toggle-switch name="active" :checked="false" inline/>
 
+					<x:form::textarea id="custom-id" name="first_name"/>
+					<x:form::input name="name" caption="Please fill this input with your full name." />
+				</div>
 			</div>
-		</div>
-	</form>
-
+			<div class="card-footer text-end">
+				<div class="d-flex">
+					<button type="submit" class="btn btn-primary me-auto">Send data</button>
+					<a href="#" class="btn btn-link">Cancel</a>
+				</div>
+			</div>
+		</form>
+	</div>
 @endsection
--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -12,16 +12,42 @@
 			</div>
 			<div class="col-auto ms-auto">
 				<div class="btn-list">
-        <span class="d-none d-sm-inline">
-          <a href="#" class="btn btn-white">
-            New view
-          </a>
-        </span>
-					<a href="#" class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal" data-bs-target="#modal-report">
-						Create new report
+					<a href="#" class="btn btn-primary">
+						ایجاد
 					</a>
-					<a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal" data-bs-target="#modal-report" aria-label="Create new report">
+					<a href="#" class="btn btn-white" data-bs-toggle="modal" data-bs-target="#modal-report">
+						جستجو
 					</a>
+					<div class="modal modal-blur fade" id="modal-report" tabindex="-1" role="dialog" aria-hidden="true">
+						<div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h5 class="modal-title">جستجو</h5>
+									<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+								</div>
+
+								<div class="modal-body">
+									<div class="row">
+										<x:form::input placeholder="salam" class="date" name="navid"/>
+										<x:form::input placeholder="salam" Disabled name="navid"/>
+										<x:form::input type="password" placeholder="salam" name="navid"/>
+										<x:form::checkbox name="newsletter_subscription" :checked="true"/>
+										@php($options = [1 => 'Laravel', 2 => 'Bootstrap', 3 => 'Tailwind', 4 => 'Livewire'])
+										<x:form::select name="hobby" placeholder="What is your hobby prefered hobby?" :options="$options" selected="1"/>
+									</div>
+								</div>
+								<div class="modal-footer">
+									<a href="#" class="btn btn-primary me-auto">
+										جستجو
+									</a>
+									<a href="#" class="btn btn-link link-secondary" data-bs-dismiss="modal">
+										لغو
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+
 				</div>
 			</div>
 		</div>
