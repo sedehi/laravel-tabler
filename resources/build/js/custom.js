9b79d0af3111e62cfbb7eb5e006851493c2f968a
navid <navid.sedehi@gmail.com>
2023-06-04 16:36:00 +0330

    wip

--- resources/build/js/custom.js
+++ resources/build/js/custom.js
@@ -35,3 +35,34 @@ const Toast = Swal.mixin({
     timer: 1500,
     timerProgressBar: true
 })
+
+let options = {
+    selector: '.editor',
+    height: 300,
+    menubar: true,
+    statusbar: false,
+    directionality: $('html').attr('dir'),
+    language: $('html').attr('lang'),
+    font_family_formats: 'Arial=arial,helvetica,sans-serif; Tahoma=tahoma,monospace; Vazir=vazir',
+    plugins: [
+        'code',
+        'fullscreen',
+        'advlist',
+        'autolink',
+        'lists',
+        'link',
+        'image',
+        'charmap',
+        'preview',
+        'anchor',
+        'media',
+        'table',
+        'wordcount',
+        'codesample',
+    ],
+    toolbar: 'undo redo | formatselect | ' +
+        'bold italic backcolor | alignleft aligncenter ' +
+        'alignright alignjustify | bullist numlist outdent indent | ' +
+        'removeformat',
+}
+tinyMCE.init(options);
