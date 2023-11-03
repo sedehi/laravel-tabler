dacaf2f1eb9d84ddd71f216cd62c8413c644f5f6
Navid Sedehi <navid.sedehi@gmail.com>
2023-11-03 20:30:02 +0330

    Update head.blade.php

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,7 +5,11 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>@yield('title',trans('permissions.'.request()->route()->getName()))</title>
 	<link href="{{asset('admin/css/Vazirmatn-Variable-font-face.css')}}" rel="stylesheet"/>
-	<link href="{{asset('admin/css/tabler.rtl.min.css')}}" rel="stylesheet"/>
+	@if(in_array(app()->getLocale(),['fa','ar']))
+		<link href="{{asset('admin/css/tabler.rtl.min.css')}}" rel="stylesheet"/>
+	@else
+		<link href="{{asset('admin/css/tabler.min.css')}}" rel="stylesheet"/>
+	@endif
 	<link href="{{asset('admin/css/sweetalert2.min.css')}}" rel="stylesheet"/>
 	<link href="{{asset('admin/css/custom.css')}}" rel="stylesheet"/>
 	@stack('css')
